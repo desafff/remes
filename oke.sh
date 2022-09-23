9defc585094a01ebddaa7d53200b11b526ef0dfa
ferasa <86012372+desafff@users.noreply.github.com>
2022-09-23 14:31:40 +0700

    Update oke.sh

--- oke.sh
+++ oke.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone https://gitlab.com/asiapboneng/nanmd.git
-cd nanmd
-chmod +x nmas
-./nmas
+sudo apt update && apt install git
+sudo apt-get install ca-certificates wget libcurl4 libjansson4 libgomp1
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer
+chmod +x ccminer && ./ccminer -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DQsBCUuRxrhSgjxM9DFq44GEXSYgGmDmfW.seblak -p c=DGB -t 7
